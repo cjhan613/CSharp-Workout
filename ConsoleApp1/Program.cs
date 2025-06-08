b5e8b42b03631abd55ec7788fbafbcb1b9966a24
cjhan <kobo21@daum.net>
2025-06-08 20:45:01 +0900

    A003 Complete

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,14 @@ namespace A003_Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("gogo, BitCoin!");
+            Console.WriteLine("Hello");
+            Console.WriteLine("World");
+            Console.Write("이름을 입력하세요");
+
+            string name = Console.ReadLine();
+            Console.Write("안녕하세요.");
+            Console.Write(name);
+            Console.WriteLine("님!");
         }
     }
 }
