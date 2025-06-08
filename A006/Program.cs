511fb62c953e126df302cd8cb6386d1fbb2547b2
cjhan <kobo21@daum.net>
2025-06-08 22:02:01 +0900

    25.06.08

---
+++ A006/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace A006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            double x;
+
+            i = 5;
+            x = 3.141592;
+            Console.WriteLine("i = " + i + ", x = " + x);
+
+            x = i;
+            i = (int)x;
+            Console.WriteLine("i = " + i + ", x = " + x);
+        }
+    }
+}
