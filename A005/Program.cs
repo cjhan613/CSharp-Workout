4dd77edcbb2aff7e532e930c30cb4a4cdc03ce83
cjhan <kobo21@daum.net>
2025-06-08 21:10:11 +0900

    A005 Complete

---
+++ A005/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace A005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "hello";
+            string b = "h";
+
+            b = b + "ello";
+
+            Console.WriteLine(a==b);
+            Console.WriteLine("b="+b);
+
+            int x = 10;
+            string c = b + '!' + " " + x;
+            Console.WriteLine("c=" + c);
+        }
+    }
+}
