e79f3dc6446cae80de4ca4beaa926a5d8b84905d
cjhan <kobo21@daum.net>
2025-06-08 20:54:19 +0900

    A004 Complete

--- ExampleProject/Program.cs
+++ ExampleProject/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Threading.Channels;
 
-namespace A003_Console
+namespace A004_Variable
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, cj!");
+            Console.Write("이름을 입력하세요");
+            string name = Console.ReadLine();
+            Console.Write("나이를 입력하세요");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("키를 입력하세요");
+            float Height = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"안녕하세요{name}님!");
+            Console.WriteLine($"나이는 : {age}");
+            Console.WriteLine($"키는 : {Height}cm");      
+            
         }
     }
 }
