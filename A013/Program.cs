9ede0b434457ddf9ef8d271c7e4d45a1a55b20a7
cjhan <kobo21@daum.net>
2025-06-10 21:31:46 +0900

    A014 25.06.10

---
+++ A013/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Channels;
+
+
+namespace A013
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 2147483647;
+            long bigNum = num;
+            Console.WriteLine("{0},{1}", bigNum, num);
+
+
+            double x = 1234.5;
+            int a;
+
+            a = (int)x;
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ A014/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace A014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int value;
+
+            Console.WriteLine("1. int로 변환할 문자열을 입력하세요");
+            input = Console.ReadLine(); // 문자열 입력
+            bool result = Int32.TryParse(input, out value);
+
+            if (!result)
+            {
+                Console.WriteLine("{0}는 int로 변환될 수 없습니다.\n", input);
+            }
+
+            else
+            {
+                Console.WriteLine("{0}는 input로 변환 되었습니다 \n", input);
+            }
+
+            Console.WriteLine("2. double 로 변환할 문자열을 입력하세요");
+            
+            input = Console.ReadLine(); // 문자열 입력
+
+            try
+            {
+                double m = Double.Parse(input);
+                Console.WriteLine("double{0}으로 변환 되었습니다", input);
+            }
+
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }          
+
+        }
+    }
+}
